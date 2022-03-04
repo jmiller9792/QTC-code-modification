ae6eefe4c19eb44dc6812f6f1e68caa1a103fbca
Justin <35930068+jmiller9792@users.noreply.github.com>
2022-03-04 12:39:22 -0500

    Create SetWorkingDirectories.m

---
+++ SetWorkingDirectories.m
@@ -0,0 +1,15 @@
+
+computer = 'JDMLaptop';
+
+cd Z:\equip\QTC\Programs
+
+if strcmp(computer,'JDMLaptop')
+    addpath('C:\Users\jmill\OneDrive - purdue.edu\Documents\GitHub\QTC-code-modification')
+    addpath('C:\Users\jmill\OneDrive - purdue.edu\Documents\GitHub\QTC-code-modification\cnc_edit')
+elseif strcmp(computer,'Purdue')
+    addpath('U:\My Documents\GitHub\QTC-code-modification')
+    addpath('U:\My Documents\GitHub\QTC-code-modification\cnc_edit')
+end
+
+
+    
