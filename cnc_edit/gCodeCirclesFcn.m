2a1599d9889c08067612fc5ef4d9e56232bb2290
Justin <35930068+jmiller9792@users.noreply.github.com>
2022-03-07 16:28:20 -0500

    Fix circle direction issues
    Code was deleted during a github update which needed to be replaced
    
    Other organizational changes were also made with added comments for clarity

--- cnc_edit/gCodeCirclesFcn.m
+++ cnc_edit/gCodeCirclesFcn.m
@@ -6,15 +6,17 @@ errorAccept = 0.01;
 % G02 line of G-code. Using I and J values is just one of several ways to
 % define an arc.
 
-% Rotation Direction: G02 = CW,  G03 = CCW
+% Set Rotation Direction: G02 = CW,  G03 = CCW
 if strcmp(line.type,'cwCircle')
     codeDir = 'G02';
 elseif strcmp(line.type,'ccwCircle')
     codeDir = 'G03';
 else
+    % if not a circular motion, exit function
     return
 end
 
+% Set plane of circular interpolation
 if strcmp(line.circInterp,'xz')
     x1 = line.coordLast.Z;
     y1 = line.coordLast.X;
@@ -38,25 +40,19 @@ else % Default xy
     jCurrent = line.coord.J;
 end
 
-% x1 = line.coordLast.X;
-% y1 = line.coordLast.Y;
-% x2 = line.coord.X;
-% y2 = line.coord.Y;
-
 % Get radius from line (this is used for quick changes to radius)
 comment = line.tail(2:end);
 rIndex = strfind(comment,'r');
 rIndex = [rIndex,strfind(comment,'R')];
 if ~isempty(rIndex)
     r = getVal2(comment,rIndex);
-    
-else
+else % if not radius is not specified in line, no circle interpolation is performed. 
     disp([line.lineNum,' ERROR: No radius found'])
     return
 end
 
 
-if ~isempty(iCurrent) % If already specified, check that radius is consistent
+if ~isempty(iCurrent) % If i and j already specified, check that radius is consistent
     % Find center coordinates
     xcCurrent = x1+iCurrent;
     ycCurrent = y1+jCurrent;
@@ -92,53 +88,92 @@ if (2*r) <= d
    return
 end
 
+
+% x_m = (x2+x1)/2;
+% y_m = (y2+y1)/2;
+% m_mid = -(x2-x1)/(y2-y1); %perpendicular to chord between start and end points
+
+
+
+% Radius (make sure it is big enough to span the two points)
+d = sqrt((x1-x2)^2+(y1-y2)^2);
+if (2*r) <= d
+   disp('You done messed up. Make the radius bigger')
+   return
+end
+
 x_m = (x2+x1)/2;
 y_m = (y2+y1)/2;
 m_mid = -(x2-x1)/(y2-y1); %perpendicular to chord between start and end points
 
+b_mid = y_m-m_mid*x_m;
+% ymcheck = m_mid*x_m+b_mid
 
+% Revised solve for single equation improves accuracy significantly
+syms xc yc
+test = r-sqrt((xc-x1)^2 + ((m_mid*xc + b_mid)-y1)^2);
+T = solve(test,xc);
+xC1 = double(T(1)); %Two possible results are computed
+xC2 = double(T(2));
+yC1 = m_mid*xC1 + b_mid;
+yC2 = m_mid*xC2 + b_mid;
 
-    % Radius (make sure it is big enough to span the two points)
-    d = sqrt((x1-x2)^2+(y1-y2)^2);
-    if (2*r) <= d
-       disp('You done messed up. Make the radius bigger')
-       return
-    end
+% test(1) = r == sqrt((xc-x1)^2 + (yc-y1)^2);
+% test(2) = yc == m_mid*xc + b_mid;
+% circr = @(xc)  r-sqrt((xc-x1)^2 + ((m_mid*xc + b_mid)-y1)^2)
 
-    x_m = (x2+x1)/2;
-    y_m = (y2+y1)/2;
-    m_mid = -(x2-x1)/(y2-y1); %perpendicular to chord between start and end points
+% T = solve(test,[xc;yc])
 
-    b_mid = y_m-m_mid*x_m;
-    % ymcheck = m_mid*x_m+b_mid
+% xC1 = double(T.xc(1));
+% yC1 = double(T.yc(1));
+% xC2 = double(T.xc(2));
+% yC2 = double(T.yc(2));
 
-    syms xc yc
-    test = r-sqrt((xc-x1)^2 + ((m_mid*xc + b_mid)-y1)^2);
-    T = solve(test,xc);
-    xC1 = double(T(1));
-    xC2 = double(T(2));
-    yC1 = m_mid*xC1 + b_mid;
-    yC2 = m_mid*xC2 + b_mid;
+% Determine angles between points to determine direction traveled
+[theta_center1,~] = cart2pol(xC1-x1,yC1-y1); % get polar angle from start point (point 1) to center point option 1
+[theta_center2,~] = cart2pol(xC2-x1,yC2-y1); %get polar angle from start point (point 1) to center point option 2
+[theta2,~] = cart2pol(x2-x1,y2-y1); % get polar angle from start point (point 1) to end point (point 2)
+% Note: cart2pol outputs -pi to pi
+% NOTE: directions are reverse in plots
 
-    % test(1) = r == sqrt((xc-x1)^2 + (yc-y1)^2);
-    % test(2) = yc == m_mid*xc + b_mid;
-    % circr = @(xc)  r-sqrt((xc-x1)^2 + ((m_mid*xc + b_mid)-y1)^2)
+thetadif1 = theta_center1-theta2;
+thetadif2 = theta_center2-theta2;
 
-    % T = solve(test,[xc;yc])
+%% Previously deleted from code via github
+%Correct for out of bounds values
+if thetadif1>=pi/2
+    thetadif1 = thetadif1-2*pi;
+elseif thetadif1<=-pi/2
+    thetadif1 = thetadif1+2*pi;
+end
+if thetadif2>=pi/2
+    thetadif2 = thetadif2-2*pi;
+elseif thetadif2<=-pi/2
+    thetadif2 = thetadif2+2*pi;
+end
 
-    % xC1 = double(T.xc(1));
-    % yC1 = double(T.yc(1));
-    % xC2 = double(T.xc(2));
-    % yC2 = double(T.yc(2));
-
-
-    [theta_center1,~] = cart2pol(xC1-x1,yC1-y1);
-    [theta_center2,~] = cart2pol(xC2-x1,yC2-y1);
-    [theta2,~] = cart2pol(x2-x1,y2-y1);
-
-    thetadif1=theta_center1-theta2;
-    thetadif2=theta_center2-theta2;
-
+% Determine which solution corresponds to which direction
+if thetadif1>=0
+    xC_g03 = xC1;
+    yC_g03 = yC1;
+    xC_g02 = xC2;
+    yC_g02 = yC2;
+else
+    xC_g02 = xC1;
+    yC_g02 = yC1;
+    xC_g03 = xC2;
+    yC_g03 = yC2;    
+end
+% Assign 
+if strcmp(codeDir,'G02')
+    xC = xC_g02;
+    yC = yC_g02;
+else
+    xC = xC_g03;
+    yC = yC_g03;
+end
+%%
+% Compute I and J from center point and previous point. 
 I = xC-x1;
 J = yC-y1;
 % disp(codeDir+ " X" + num2str(x2) + " Y" + num2str(y2) + " I" + num2str(I) + " J" + num2str(J)+ " (R="+ num2str(r)+ ")");
