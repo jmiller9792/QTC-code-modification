51375921dcd883509d0895083fdfc8fb2e98387f
Justin <35930068+jmiller9792@users.noreply.github.com>
2022-03-12 15:16:28 -0500

    Implement Z limits incomplete

--- cnc_edit/writeLine.m
+++ cnc_edit/writeLine.m
@@ -19,16 +19,43 @@ function [lineNew] = writeLine(lineStruct)
             i = gCodeCirclesFcn(i,0); % Check circular coordinates
             lineNew = [lineNew,' G',i.gNum];
             if ~isempty(i.coord)
-                coordList = fieldnames(i.coord);
+                coordList = {'X','Y','Z','B','C','I','J','K'};
                 for j = 1:length(coordList)
-                    if ~isnan(i.coord.(coordList{j})) 
-                        lineNew = [lineNew,' ',coordList{j},'=',num2str(i.coord.(coordList{j}),'%.3f')];
-                    else
-                        disp(['error non-numeric line: ',i.lineNum, ' ', coordList{j}])
-                        return
+                    if isfield(i.coord,coordList{j}) % Check that coordinate exists in given line
+                        if ~isnan(i.coord.(coordList{j}))  % Check that field has valid balue
+                            if strcmp(coordList{j},'Z') % Check out of bounds
+                                if i.coord.Z>40
+                                    i.coord.Z = 40;
+                                    i = gCodeCirclesFcn(i,0); % ReCheck circular coordinates
+                                    disp([i.lineNum,': Z out of bounds, corrected to 40'])
+                                elseif i.coord.Z<-120
+                                    i.coord.Z = -120;
+                                    i = gCodeCirclesFcn(i,0); % ReCheck circular coordinates
+                                    disp([i.lineNum,': Z out of bounds, corrected to -120'])
+                                end
+                            end
+                            lineNew = [lineNew,' ',coordList{j},'=',num2str(i.coord.(coordList{j}),'%.3f')];
+                        else
+                            disp(['error non-numeric line: ',i.lineNum, ' ', coordList{j}])
+                            return
+                        end
                     end
-                    
                 end
+%                 if ~isnan(i.coord.X)
+%                     lineNew = [lineNew,' X=',num2str(i.coord.(coordList{j}),'%.3f')];
+                    
+                    
+%                 coordList = fieldnames(i.coord);
+%                 for j = 1:length(coordList)
+%                     if ~isnan(i.coord.(coordList{j})) 
+%                         lineNew = [lineNew,' ',coordList{j},'=',num2str(i.coord.(coordList{j}),'%.3f')];
+%                     else
+%                         disp(['error non-numeric line: ',i.lineNum, ' ', coordList{j}])
+%                         return
+%                     end
+%                     
+%                 end
+
             end
         end
         lineNew = [lineNew,i.tail];
